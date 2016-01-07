76d92bc8cf530cfc927759d31177901df9c49aca
Erin Mahon <erin.mahon14@gmail.com>
2016-01-07 12:03:05 -0500

    fixed issue with topics table migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160107164600) do
+ActiveRecord::Schema.define(version: 20160106215808) do
 
   create_table "article_topics", force: :cascade do |t|
     t.integer  "topic_id"
@@ -58,7 +58,10 @@ ActiveRecord::Schema.define(version: 20160107164600) do
     t.datetime "updated_at", null: false
   end
 
-# Could not dump table "topics" because of following NoMethodError
-#   undefined method `[]' for nil:NilClass
+  create_table "topics", force: :cascade do |t|
+    t.string   "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
