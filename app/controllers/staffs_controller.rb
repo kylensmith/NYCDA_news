b45a04754e9978e69aa9fa9d5492ac47bceeecc2
Kyle N. Smith <smith.kylen@gmail.com>
2016-01-07 11:52:50 -0500

    Updates to destroy link for staff

--- app/controllers/staffs_controller.rb
+++ app/controllers/staffs_controller.rb
@@ -5,12 +5,22 @@ class StaffsController < ApplicationController
   end
 
   def new
+    @staff = Staff.new
   end
 
   def create
+    @staff = Staff.create(staff_params)
+    if @staff.save
+      flash[:notice] = "Your account has been created."
+      redirect_to staffs_path
+    else
+      flash[:alert] = "There was a problem saving your account."
+      redirect_to staffs_path_new
+    end
   end
 
   def edit
+
   end
 
   def update
@@ -20,5 +30,15 @@ class StaffsController < ApplicationController
   end
 
   def destroy
+
   end
+
+  private   
+
+# this provdes additional security by only allowing the permitted variables to be accessed and changed.
+
+  def staff_params
+    params.require(:staff).permit(:id, :fname, :lname, :email, :password, :job_title)
+  end
+
 end
--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module News
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # commented out line of code because I am now using params.require in the controller for added security via strong params.
+    # config.action_controller.permit_all_parameters = true
   end
 end
