300197cd5e0614ecd41a2c04e37a9f86d8a02e08
Kyle N. Smith <smith.kylen@gmail.com>
2016-01-08 15:29:09 -0500

    added login sessions and pictures for the dummy database

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,42 +1,48 @@
 class SessionsController < ApplicationController
-	enable :sessions
-	set :sessions, true
+	# enable :sessions
+	# set :sessions, true
 
 	def create
 
 
-		@staff = Staff.find_by_email(params[:email]) 
-		if @staff and @staff.password == params[:password]
-			session[:staff_id] = @staff.id 
-			redirect_to article_path_new
-		else
-			flash[:alert] = "There was a problem, please try again."
-			render :new
+		@user = Staff.find_by_email(params[:email]) 
+		if @user and @user.password == params[:password]
+			session[:staff_id] = @user.id 
+			redirect_to articles_path
+		else 
+			@user = Subscriber.find_by_email(params[:email]) 
+			if @user and @user.password == params[:password]
+				session[:subscriber_id] = @user.id 
+				redirect_to subscribers_path
+			else
+				flash[:alert] = "There was a problem, please try again."
+				render :new
+			end
 		end
 	end
 
-	def create_subscriber
+	# def create_subscriber
 
 
-		@subscriber = Subscriber.find_by_email(params[:email]) 
-		if @subscriber and @subscriber.password == params[:password]
-			session[:subscriber_id] = @subscriber.id 
-			redirect_to root_path
-		else
-			flash[:alert] = "There was a problem, please try again."
-			render :new
-		end
-	end
+	# 	@subscriber = Subscriber.find_by_email(params[:email]) 
+	# 	if @subscriber and @subscriber.password == params[:password]
+	# 		session[:subscriber_id] = @subscriber.id 
+	# 		redirect_to root_path
+	# 	else
+	# 		flash[:alert] = "There was a problem, please try again."
+	# 		render :new
+	# 	end
+	# end
 
-	def destroy_staff
-		session[:staff_id] = nil
-		redirect_to root_path, notice "You have been logged out."
-	end
+	# def destroy_staff
+	# 	session[:staff_id] = nil
+	# 	redirect_to root_path, notice "You have been logged out."
+	# end
 
-	def destroy_subscriber
-		session[:subscriber_id] = nil
-		redirect_to root_path, notice "You have been logged out."
+	# def destroy_subscriber
+	# 	session[:subscriber_id] = nil
+	# 	redirect_to root_path, notice "You have been logged out."
 
-	end
+	# end
 
 end
